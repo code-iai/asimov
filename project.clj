11c5644d5f93bc4b2f0d89558bba3b065c388283
Jan-Paul Bultmann <ticking@me.com>
2014-05-26 14:51:09 +0200

    Filled out template, with namespaces and deps.

--- project.clj
+++ project.clj
@@ -3,4 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.async "0.1.303.0-886421-alpha"]
+                 [ring "1.2.2"]
+                 [compojure "1.1.8"]
+                 [http-kit "2.1.16"]
+                 [necessary-evil "2.0.0"]
+                 [gloss "0.2.2"]])
