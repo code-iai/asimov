a86493ceb2ab3baa2ac9bb77acc9b746f43d1257
Maik Schünemann <maikschuenemann@gmail.com>
2014-05-26 21:37:29 +0200

    started implementing grammar for message definitions

--- src/asimov/messages.clj
+++ src/asimov/messages.clj
@@ -1,9 +1,29 @@
 (ns asimov.messages
   (:require [instaparse.core :as insta]))
 
+
+(def simple-parser (insta/parser "S = 'a' | 'b'"))
+
 (def msg-parser
   (insta/parser
-    "S = FIELD
-     FIELD = TYPE NAME
-     TYPE = 'bool' |
-     NAME = 'b'+")
+   " S = (field <#'[\\n ]'*>)*
+     field = type <' '>* name
+     name = #'[a-zA-Z_-]'*
+     type = type <'[]'>
+     array-type = type <'[]'>
+     type = primitive-type | msg-type 
+     msg-type = (name <'/'>)+ name
+     primitive-type = 'bool' | 'int8' | 'uint8' | 'int16' | 'uint16' | 'int32' | 'uint32' | 'int64' | 'uint64' | 'float32' | 'float64' | 'string' | 'time' | 'duration'
+"))
+
+
+(defn transform-parse [parse-res]
+  (insta/transform
+   {:field (fn [type name]
+             {:type type
+              :name name})
+    :primitive-type identity
+    
+    :S (fn [& args] (vec args))
+    :name #(symbol (apply str %&))}
+   parse-res))
--- src/asimov/slave.clj
+++ src/asimov/slave.clj
@@ -1,1 +1,60 @@
-(ns asimov.slave)
+(ns asimov.slave
+  (require
+   [org.httpkit.server :as server]
+   [compojure
+    [core :as compojure :refer [defroutes GET POST]]
+    [handler :refer [api]]]
+   [necessary-evil.core :as xml-rpc]))
+
+
+(def ros-url "http://192.168.56.101:11311/")
+
+
+bool (1)
+unsigned 8-bit int
+uint8_t (2)
+bool
+int8
+signed 8-bit int
+int8_t
+int
+uint8
+unsigned 8-bit int
+uint8_t
+int (3)
+int16
+signed 16-bit int
+int16_t
+int
+uint16
+unsigned 16-bit int
+uint16_t
+int
+int32
+signed 32-bit int
+int32_t
+int
+uint32
+unsigned 32-bit int
+uint32_t
+int
+int64
+signed 64-bit int
+int64_t
+long
+uint64
+unsigned 64-bit int
+uint64_t
+long
+float32
+32-bit IEEE float
+float
+float
+float64
+64-bit IEEE float
+double
+float
+string
+ascii string (4)
+std::string
+string
