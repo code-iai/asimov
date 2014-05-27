c75347e8938f434cb9458331a9e34074af75a29e
Jan-Paul Bultmann <ticking@me.com>
2014-05-27 21:04:51 +0200

    Reworked parser to parse most known message cases.

--- src/asimov/messages.clj
+++ src/asimov/messages.clj
@@ -1,65 +1,85 @@
 (ns asimov.messages
   (:require [instaparse.core :as insta]))
 
-
-(def simple-parser (insta/parser "S = 'a' | 'b'"))
-
-(def double-regex #"[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?")
-
 (def msg-parser
   (insta/parser
-   " S = (field <#'[\\n ]'*>)*
-     <field> = constant-field | variable-field
-     constant-field = numeric-constant-field | string-constant-field | bool-constant-field
-     variable-field = type <' '>+ name 
-     name = #'[a-zA-Z_-]'+
-     <type> = primitive-type | array-type | msg-type
-     array-type = type <'[]'>
-     msg-type = name | name  '/'  name
+   " S = {declaration? <whitespace? comment? ('\\n' | #'\\z')>}
+     <declaration> = field | constant
+     <field> = unary-field | tuple-field | list-field
+     unary-field = type <whitespace> field-name
+     tuple-field = type <'['>#'[0-9]+'<']'> <whitespace> field-name
+     list-field = type <'[]'> <whitespace> field-name
+     constant = numeric-constant | string-constant | bool-constant
+     <numeric-constant> = numeric-type <whitespace> field-name <whitespace?> <'='> <whitespace?> numeric-lit
+     <string-constant>  = string-type  <whitespace> field-name <whitespace?> <'='> <whitespace?> string-lit
+     <bool-constant>    = bool-type    <whitespace> field-name <whitespace?> <'='> <whitespace?> bool-lit
+     <type> = primitive-type | msg-type
      <primitive-type> = numeric-type | string-type | bool-type | time-type
      <bool-type> = 'bool'
      <numeric-type> = 'int8' | 'uint8' | 'int16' | 'uint16' | 'int32' | 'uint32' | 'int64' | 'uint64' | 'float32' | 'float64'
      <string-type> = 'string'
      <time-type> =  'time' | 'duration'
-     <numeric-constant-field> = numeric-type <' '>+ name <'='> numeric-lit
-     <numeric-lit> = double-lit | int-lit 
-     <bool-constant-field> = bool-type <' '> name <'='> bool-lit
-     <string-constant-field> = string-type <' '> name <'='> string-lit
+     <field-name> = #'[a-zA-Z][a-zA-Z1-9_]*'
+     msg-type = #'[a-zA-Z]' #'[0-9a-zA-Z_]*' ('/' #'[0-9a-zA-Z_]*')?
+     <numeric-lit> = double-lit | int-lit
      double-lit = #'[-+]?[0-9]*.?[0-9]+([eE][-+]?[0-9]+)?'
      int-lit = #'[-+]?[0-9]*'
-     <string-lit> = #'.*'
+     string-lit = #'((?=\\S).*\\S)'?
      bool-lit = 'true' | 'false'
+     comment = '#' #'.*'
+     whitespace = #'[^\\S\\r\\n]'+
 "))
-
 
 (defn transform-parse [parse-res]
   (insta/transform
-   {:variable-field
+   {:S vector
+    :simple-field
     (fn [type name]
-      {:tag ::variable
-       :type type
+      {:type type
        :name name})
-    :constant-field
+    :unary-field
+    (fn [type name]
+      {:type type
+       :name name})
+    :tuple-field
+    (fn [type arity name]
+      {:type type
+       :name name
+       :arity (Integer/parseInt arity)})
+    :list-field
+    (fn [type name]
+      {:type type
+       :name name
+       :arity :list})
+    :constant
     (fn [type name value]
-      {:tag ::constant
-       :type type
+      {:type type
        :name name
        :value value})
-    :array-type (fn [type]
-                  {:array type})
     :double-lit #(Double/parseDouble %)
-    :int-list #(Integer/parseInt %)
+    :int-lit #(Integer/parseInt %)
     :bool-lit (fn [bl] (case bl "true" true "false" false))
-    :S (fn [& args] (vec args))
-    :msg-type #(apply str %&)
-    :name #(apply str %&)}
+    :string-lit str
+    :msg-type str}
    parse-res))
-
 
 (defn parse-message [msg]
   (-> msg
       msg-parser
       transform-parse))
+
+(midje/tabular "Different message parts."fjkdlaöfkldjöflkadjf
+   (midje/fact (parse ?msg) => ?res)
+   ?msg                                            ?res
+   "# Data"                                        []
+   "int32 id"                                      [{:type "int32"  :name "id"}]
+   "byte mode   # or-combination of values to set" [{:type "byte"   :name "mode"}]
+   "geometry_msgs/PoseStamped value_posestamped"   [{:type "geometry_msgs/PoseStamped"  :name "value_posestamped"}]
+   "char[] value_data"                             [{:type "char"   :name "value_data"  :arity :list}]
+   "char[4] value_data"                            [{:type "char"   :name "value_data"  :arity 4}]
+   "int32 TYPE_STRING=0"                           [{:type "int32"  :name "TYPE_STRING" :value 0}]
+   "string EXAMPLE=\"#comments\" are ignored"      [{:type "string" :name "EXAMPLE"     :value "\"#comments\" are ignored"}]
+   "string EXAMPLE=  \t border wspace removed \t " [{:type "string" :name "EXAMPLE"     :value "border wspace removed"}])
 
 ;;Status: variable and constant fields are recognized
 ;;TODO: - more type checking
