985044e9b7c66c3132cce05abd7553be1835592f
Maik Schünemann <maikschuenemann@gmail.com>
2014-06-01 22:36:11 +0200

    started working on implementing slave api and communicating with master

--- src/asimov/master.clj
+++ src/asimov/master.clj
@@ -1,1 +1,32 @@
-(ns asimov.master)
+(ns asimov.master
+  (require
+   [compojure
+    [core :as compojure :refer [defroutes GET POST]]
+    [handler :refer [api]]]
+   [necessary-evil.core :as xml-rpc]))
+
+;;for testing locally
+(def ^:dynamic *ros-master-url* "http://192.168.56.101:11311/")
+
+
+(xml-rpc/call *ros-master-url* :getSystemState "/")
+
+(defn gen-return-map [code status-message]
+  {:success? (if (= 1 code) true false)
+   :status-code code
+   :status-message status-message})
+
+
+(defn register-subscriber
+  ([subscriber-node topic msg subscriber-url]
+     (register-subscriber *ros-master-url* subscriber-node
+                          topic msg subscriber-url))
+  ([master-url subscriber-node topic msg subscriber-url]
+     (let [[code message provider-url]
+           (xml-rpc/call master-url :registerSubscriber
+                         subscriber-node topic msg subscriber-url)]
+       (-> (gen-return-map code message)
+           (assoc :provider-url provider-url)))))
+
+
+(defn request-topic [])
