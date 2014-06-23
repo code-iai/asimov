08b95a4caeedb9261000abe47018d0e223e11264
Jan-Paul Bultmann <ticking@me.com>
2014-06-23 14:59:04 +0200

    Adds raw tcpros subscription code.

--- src/asimov/tcpros.clj
+++ src/asimov/tcpros.clj
@@ -1,19 +1,46 @@
 (ns asimov.tcpros
   (:require [lamina.core :as l]
             [aleph.tcp   :as a]
-            [gloss.core  :as g]))
+            [gloss.core  :as g]
+            [gloss.io    :as i]
+            [taoensso.timbre :as t]))
 
-(def header
+(alter-var-root #'*out* (constantly *out*))
+
+(def header-frame
   (g/finite-frame :uint32-le
                   (g/repeated (g/finite-frame :uint32-le
                                               [(g/string :ascii :delimiters [\=])
                                                (g/string :ascii)])
                               :prefix :none)))
 
-#_(defn handler [ch client-info]
-    (receive-all ch
-                 #(enqueue ch (str "You said " %))))
+(defn handler [ch client-info]
+  (l/siphon ch ch))
 
-#_(defn listen! []
-    (start-tcp-server handler {:port 10000, :frame
-                               (string :utf-8 :delimiters ["\r\n"])}))
+(a/start-tcp-server handler {:port 10000})
+
+(defn encode-header [h]
+  (->> h
+       (map (fn [[k v]] [(name k) v]))
+       (into [])
+       (i/encode header-frame)))
+
+(defn decode-header [ch]
+  (let [ch* (i/decode-channel-headers ch header-frame)
+        h (i/read-channel ch*)]
+    [ch* (future (->> @h l/wait-for-message
+                      (map (fn [[k v]] [(keyword k) v]))
+                      (into {})))]))
+
+(defn subscribe [host port callerid topic msg]
+  (let [ch> (-> {:host host
+                 :port port}
+                a/tcp-client
+                l/wait-for-result)
+        [ch< inh] (decode-header (l/map* (memfn toByteBuffer) ch>))]
+    (i/enqueue ch> (encode-header outh {:message_definition (:cat msg)
+                                        :callerid callerid
+                                        :topic topic
+                                        :md5sum (:md5 msg)
+                                        :type (str (:package msg) "/" (:name msg))}))
+    (println @inh)))
