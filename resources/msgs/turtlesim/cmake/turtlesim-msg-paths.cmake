6076d29f6756e6853dcd3d04edaecdf61c4ff48a
Jan-Paul Bultmann <ticking@me.com>
2014-07-28 13:52:49 +0200

    Removed unnessecary msg defs.

--- resources/msgs/turtlesim/cmake/turtlesim-msg-paths.cmake
+++
@@ -1,5 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.em
-
-# message include dirs in installspace
-_prepend_path("${turtlesim_DIR}/.." "msg" turtlesim_MSG_INCLUDE_DIRS UNIQUE)
-set(turtlesim_MSG_DEPENDENCIES std_msgs;std_srvs)
